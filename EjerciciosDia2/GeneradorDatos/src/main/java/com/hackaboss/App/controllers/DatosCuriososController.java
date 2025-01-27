acd25d0a960f29457d792d044318507853b3cffe
LuciaHub <luciaprogramacion11@gmail.com>
2025-01-27 20:31:49 +0100

    cambios en generadorDatos

--- EjerciciosDia2/GeneradorDatos/src/main/java/com/hackaboss/App/controllers/DatosCuriososController.java
+++ EjerciciosDia2/GeneradorDatos/src/main/java/com/hackaboss/App/controllers/DatosCuriososController.java
@@ -4,15 +4,27 @@ import com.hackaboss.App.entities.DatosCuriosos;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 @RestController
 public class DatosCuriososController {
+    private static List<DatosCuriosos> datosCuriosos = Arrays.asList(
+            new DatosCuriosos(1, "Los delfines tienen la capacidad de reconocer y recordar estos silbidos específicos incluso después de mucho tiempo"),
+            new DatosCuriosos(2, "Las hormigas son capaces de llevar objetos que son hasta 50 veces su propio peso"),
+            new DatosCuriosos(3, "El corazón de un camarón está en su cabeza"),
+            new DatosCuriosos(4, "Los koalas duermen alrededor de 22 horas al día"),
+            new DatosCuriosos(5, "Las estrellas de mar no tienen cerebro")
+    );
+
+    public static List<DatosCuriosos> getDatosCuriosos() {
+        return datosCuriosos;
+    }
 
     @GetMapping("/datos")
-    public String datosCurioso() {
-        List<String> datosCuriosos = DatosCuriosos.datosCuriosos();
+    public DatosCuriosos getDatosCurioso() {
+        List<DatosCuriosos> datosCuriosos = DatosCuriosos.getDatosCuriosos();
         Random random = new Random();
         int index = random.nextInt(datosCuriosos.size());
         return datosCuriosos.get(index);
